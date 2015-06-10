aed6d5c330fb9535a86cbf1420e1c4a832c43602
arter97 <qkrwngud825@gmail.com>
2015-06-11 00:13:36 +0900

    Introduce Adreno idler for devfreq-based Adreno devices
    Adreno idler by arter97 - Idling algorithm,
    an efficient workaround for msm-adreno-tz's overheads.
    
    Main goal is to lower the power consumptions while maintaining high-performance.
    
    Since msm-adreno-tz tends to *not* use the lowest frequency even on idle,
    Adreno idler replaces msm-adreno-tz's algorithm when it comes to
    calculating idle frequency(mostly by ondemand's method).
    The higher frequencies are not touched with this algorithm, so high-demanding
    games will (most likely) not suffer from worsened performance.
    
    The additional idle_lasttime detects if last 500ms was idle before
    ramping down the frequency to prevent micro-lags on scrolling or playing games.
    
    Signed-off-by: arter97 <qkrwngud825@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -373,6 +373,12 @@ static inline int devfreq_get_freq_level(struct devfreq *devfreq,
 }
 
 static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
+
+#ifdef CONFIG_ADRENO_IDLER
+extern int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
+		 unsigned long *freq);
+#endif
+
 {
 	int result = 0;
 	struct devfreq_msm_adreno_tz_data *priv = devfreq->data;
@@ -388,7 +394,21 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
 		return result;
 	}
 
+	/* Prevent overflow */
+	if (stats.busy_time >= (1 << 24) || stats.total_time >= (1 << 24)) {
+		stats.busy_time >>= 7;
+		stats.total_time >>= 7;
+	}
+
 	*freq = stats.current_frequency;
+
+#ifdef CONFIG_ADRENO_IDLER
+	if (adreno_idler(stats, devfreq, freq)) {
+		/* adreno_idler has asked to bail out now */
+		return 0;
+	}
+#endif
+
 	priv->bin.total_time += stats.total_time;
 #if 1
 	// scale busy time up based on adrenoboost parameter, only if MIN_BUSY exceeded...
