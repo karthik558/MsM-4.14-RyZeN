e511e682181fdae9a8586b37a80d52401497dc3e
celtare21 <celtare21@gmail.com>
2019-06-21 21:51:16 +0300

    alarmtimers: Don't fail on wakeup
    Userspace abuses alarmtimers, let's relax the check and not fail suspend.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Jago Gardiner (nysadev) <jagogardiner@gmail.com>
    Signed-off-by: nysascape <nysadev@raphielgang.org>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>
    Signed-off-by: karthik558 <karthik.lal558@gmail.com>

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -311,10 +311,8 @@ static int alarmtimer_suspend(struct device *dev)
 	if (min == 0)
 		return 0;
 
-	if (ktime_to_ns(min) < NSEC_PER_SEC / 2) {
+	if (ktime_to_ns(min) < NSEC_PER_SEC / 2)
 		__pm_wakeup_event(ws, MSEC_PER_SEC / 2);
-		return -EBUSY;
-	}
 
 	trace_alarmtimer_suspend(expires, type);
 
