c62f0e753b5c3be333d0d900616e9bd2668d9661
Danny Lin <danny@kdrag0n.dev>
2019-11-24 14:47:25 -0500

    cpu_input_boost: Expose compile-time settings as module parameters
    As a developer, it can be very helpful to be able to experiment with
    different values and their overall effects without constantly
    recompiling the kernel and introducing additional variables while
    wasting time.
    
    As an user, being able to tune boost values for different scenarios and
    ideals such as performance, battery, gaming, etc is very useful.
    
    Additionally, exposing these values allows us to tune them in
    performance profiles offered by the kernel that integrate into user
    interfaces such as Spectrum and FK Kernel Manager.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>
    Signed-off-by: Nishant Singh <nishant.maintainers@pixysos.com>
    Signed-off-by: depth118 <safanmuhammed1@gmail.com>
    Signed-off-by: Depth118 <safanmuhammed1@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -9,6 +9,7 @@
 #include <linux/cpufreq.h>
 #include <linux/input.h>
 #include <linux/kthread.h>
+#include <linux/moduleparam.h>
 #include <linux/msm_drm_notify.h>
 #include <linux/slab.h>
 #include <linux/version.h>
@@ -17,6 +18,28 @@
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)
 #include <uapi/linux/sched/types.h>
 #endif
+
+static unsigned int input_boost_freq_little __read_mostly =
+	CONFIG_INPUT_BOOST_FREQ_LP;
+static unsigned int input_boost_freq_big __read_mostly =
+	CONFIG_INPUT_BOOST_FREQ_PERF;
+static unsigned int max_boost_freq_little __read_mostly =
+	CONFIG_MAX_BOOST_FREQ_LP;
+static unsigned int max_boost_freq_big __read_mostly =
+	CONFIG_MAX_BOOST_FREQ_PERF;
+
+static unsigned short input_boost_duration __read_mostly =
+	CONFIG_INPUT_BOOST_DURATION_MS;
+static unsigned short wake_boost_duration __read_mostly =
+	CONFIG_WAKE_BOOST_DURATION_MS;
+
+module_param(input_boost_freq_little, uint, 0644);
+module_param(input_boost_freq_big, uint, 0644);
+module_param(max_boost_freq_little, uint, 0644);
+module_param(max_boost_freq_big, uint, 0644);
+
+module_param(input_boost_duration, short, 0644);
+module_param(wake_boost_duration, short, 0644);
 
 enum {
 	SCREEN_OFF,
@@ -50,10 +73,10 @@ static unsigned int get_input_boost_freq(struct cpufreq_policy *policy)
 	unsigned int freq;
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
-		freq = CONFIG_INPUT_BOOST_FREQ_LP;
-	else
-		freq = CONFIG_INPUT_BOOST_FREQ_PERF;
-
+		freq = input_boost_freq_little;
+	else 
+		freq = input_boost_freq_big;
+		
 	return min(freq, policy->max);
 }
 
@@ -62,10 +85,10 @@ static unsigned int get_max_boost_freq(struct cpufreq_policy *policy)
 	unsigned int freq;
 
 	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
-		freq = CONFIG_MAX_BOOST_FREQ_LP;
-	else
-		freq = CONFIG_MAX_BOOST_FREQ_PERF;
-
+		freq = max_boost_freq_little;
+	else 
+		freq = max_boost_freq_big;
+	
 	return min(freq, policy->max);
 }
 
@@ -87,9 +110,12 @@ static void __cpu_input_boost_kick(struct boost_drv *b)
 	if (test_bit(SCREEN_OFF, &b->state))
 		return;
 
+	if (!input_boost_duration)
+		return;
+
 	set_bit(INPUT_BOOST, &b->state);
 	if (!mod_delayed_work(system_unbound_wq, &b->input_unboost,
-			      msecs_to_jiffies(CONFIG_INPUT_BOOST_DURATION_MS)))
+			      msecs_to_jiffies(input_boost_duration)))
 		wake_up(&b->boost_waitq);
 }
 
@@ -225,7 +251,7 @@ static int msm_drm_notifier_cb(struct notifier_block *nb, unsigned long action,
 	/* Boost when the screen turns on and unboost when it turns off */
 	if (*blank == MSM_DRM_BLANK_UNBLANK) {
 		clear_bit(SCREEN_OFF, &b->state);
-		__cpu_input_boost_kick_max(b, CONFIG_WAKE_BOOST_DURATION_MS);
+		__cpu_input_boost_kick_max(b, wake_boost_duration);
 	} else {
 		set_bit(SCREEN_OFF, &b->state);
 		wake_up(&b->boost_waitq);
