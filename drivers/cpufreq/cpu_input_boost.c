5b037051b2e73a483d72be747c1235495f6ae889
Adam W. Willis <return.of.octobot@gmail.com>
2019-11-24 20:49:03 -0500

    cpu_input_boost: Allow configuration of each cluster's minimum idle frequency
    Because we will be using get_min_freq to raise default minimums to
    higher, more efficient frequencies, add a secondary function to
    return stock minimums when the device is idle.
    
    In this way, we can balance the utilization of more performant
    frequencies while the device is in use while retaining the ability
    to scale down to hardware minimums when it isn't.
    
    Only the little cluster minimum idle frequency will be made a
    user-configurable parameter as it's the only cluster with steps
    available which are lower than the 576 MHz stock minimum
    (300-499 MHz). Otherwise, the only purpose of these values are
    to enforce stock minimum frequencies during idle. Primary
    minimum frequency tuning will still be handled by the
    cpu_freq_min parameters.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>
    Signed-off-by: Nishant Singh <nishant.maintainers@pixysos.com>
    Signed-off-by: depth118 <safanmuhammed1@gmail.com>
    Signed-off-by: Depth118 <safanmuhammed1@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -31,6 +31,10 @@ static unsigned int cpu_freq_min_little __read_mostly =
 	CONFIG_CPU_FREQ_MIN_LP;
 static unsigned int cpu_freq_min_big __read_mostly =
 	CONFIG_CPU_FREQ_MIN_PERF;
+static unsigned int cpu_freq_idle_little __read_mostly =
+	CONFIG_CPU_FREQ_IDLE_LP;
+static unsigned int cpu_freq_idle_big __read_mostly =
+	CONFIG_CPU_FREQ_IDLE_PERF;
 
 static unsigned short input_boost_duration __read_mostly =
 	CONFIG_INPUT_BOOST_DURATION_MS;
@@ -43,6 +47,8 @@ module_param(max_boost_freq_little, uint, 0644);
 module_param(max_boost_freq_big, uint, 0644);
 module_param(cpu_freq_min_little, uint, 0644);
 module_param(cpu_freq_min_big, uint, 0644);
+module_param(cpu_freq_idle_little, uint, 0644);
+module_param(cpu_freq_idle_big, uint, 0644);
 
 module_param(input_boost_duration, short, 0644);
 module_param(wake_boost_duration, short, 0644);
@@ -106,6 +112,18 @@ static unsigned int get_min_freq(struct cpufreq_policy *policy)
 		freq = cpu_freq_min_little;
 	else
 		freq = cpu_freq_min_big;
+
+	return max(freq, policy->cpuinfo.min_freq);
+}
+
+static unsigned int get_idle_freq(struct cpufreq_policy *policy)
+{
+	unsigned int freq;
+
+	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+		freq = cpu_freq_idle_little;
+	else
+		freq = cpu_freq_idle_big;
 
 	return max(freq, policy->cpuinfo.min_freq);
 }
@@ -234,7 +252,7 @@ static int cpu_notifier_cb(struct notifier_block *nb, unsigned long action,
 
 	/* Unboost when the screen is off */
 	if (test_bit(SCREEN_OFF, &b->state)) {
-		policy->min = get_min_freq(policy);
+		policy->min = get_idle_freq(policy);
 		return NOTIFY_OK;
 	}
 
