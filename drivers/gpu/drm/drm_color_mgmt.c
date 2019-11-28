f95e4e9e7f6f29d52190e4d3b582a067f5662eda
Tanish Manku <tanish2k09.dev@gmail.com>
2019-11-28 14:10:04 -0500

    Drivers: Introduce K-lapse v5.0
    - Introducing k-lapse "flow", a brightness-based scaling mode QOL enhancement that scales RGB points by 1 smoothly instant of instantly taking effect
    - Major code refactor to better match the Linux Kernel coding style (huge thanks to kdrag0n for initial overall patch and general guidance)
    - Switched from SYS ATTR to module_param_cb and kernel param structs
    - Shortened the code from 1k lines to 0.6k without compromising any logic or feature
    - Changed tunable naming for sysfs to convey the idea of the function in a better way
    - Added minute-level support for automation. Yes, you can now start or stop at, say 16:49
    - Dimmer factor (previously known as brightness_factor) is now allowed to be set as a percentage value, ranging from 10 to 100
    - Updated README documentation to match v5.0 release
    - Integrated MDSS code with a switch in klapse.h (see kcal-mdss-demo branch for full implementation in older version)
    - Changed some default config values
    - Revised dimmer logic to fix "dimmer value multiplicative effect" for some users. The issue was changing brightness_factor values multiple times stacked them on top of each other, eventually causing the RGB to hit their minimum limit (usually 20 in KCAL), and not being able to restore full RGB without a pulse
    
    Signed-off-by: Tanish Manku <tanish2k09.dev@gmail.com>
    [kingbri: Add K-Lapse's README into Documentation/K-Lapse and move files around for a cleaner commit]
    Signed-off-by: kingbri <bdashore3@gmail.com>
    Signed-off-by: VoidDev-0 <dev.avax@protonmail.com>
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- drivers/gpu/drm/drm_color_mgmt.c
+++ drivers/gpu/drm/drm_color_mgmt.c
@@ -26,6 +26,30 @@
 
 #include "drm_crtc_internal.h"
 
+#ifdef CONFIG_KLAPSE
+#include <linux/klapse.h>
+
+unsigned short kcal_red = 256;
+unsigned short kcal_green = 256;
+unsigned short kcal_blue = 256;
+#else
+static unsigned short kcal_red = 256;
+static unsigned short kcal_green = 256;
+static unsigned short kcal_blue = 256;
+#endif
+static unsigned short kcal_hue = 0;
+static unsigned short kcal_sat = 255;
+static unsigned short kcal_val = 255;
+static unsigned short kcal_cont = 255;
+
+module_param(kcal_red, short, 0644);
+module_param(kcal_green, short, 0644);
+module_param(kcal_blue, short, 0644);
+module_param(kcal_hue, short, 0644);
+module_param(kcal_sat, short, 0644);
+module_param(kcal_val, short, 0644);
+module_param(kcal_cont, short, 0644);
+
 /**
  * DOC: overview
  *
