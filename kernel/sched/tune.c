b00aa63ce1a37760a208fcf98e5280d1186893eb
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-09-07 18:15:48 +0530

    sched/tune: Disable boost & prefer_idle if battery saver is active
    Change-Id: I495221b4a5c2239be86d796a167fa9052b11a7ec
    
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -5,6 +5,7 @@
 #include <linux/printk.h>
 #include <linux/rcupdate.h>
 #include <linux/slab.h>
+#include <linux/battery_saver.h>
 
 #include <trace/events/sched.h>
 #include <linux/list.h>
@@ -585,7 +586,7 @@ int schedtune_task_boost(struct task_struct *p)
 	struct schedtune *st;
 	int task_boost;
 
-	if (unlikely(!schedtune_initialized))
+	if (unlikely(!schedtune_initialized) || is_battery_saver_on())
 		return 0;
 
 	/* Get task boost value */
@@ -620,7 +621,7 @@ int schedtune_prefer_idle(struct task_struct *p)
 	struct schedtune *st;
 	int prefer_idle;
 
-	if (unlikely(!schedtune_initialized))
+	if (unlikely(!schedtune_initialized) || is_battery_saver_on())
 		return 0;
 
 	/* Get prefer_idle value */
@@ -636,6 +637,9 @@ static u64
 prefer_idle_read(struct cgroup_subsys_state *css, struct cftype *cft)
 {
 	struct schedtune *st = css_st(css);
+
+	if (is_battery_saver_on())
+		return 0;
 
 	return st->prefer_idle;
 }
@@ -654,6 +658,9 @@ static s64
 boost_read(struct cgroup_subsys_state *css, struct cftype *cft)
 {
 	struct schedtune *st = css_st(css);
+
+	if (is_battery_saver_on())
+		return 0;
 
 	return st->boost;
 }
