7873a094d694df4b63b3136befcd340d442e556f
karthik558 <karthik.lal558@gmail.com>
2020-12-18 09:29:34 +0000

    drivers/mmc: don't include struct_scatterlist allocs configs
    Signed-off-by: karthik558 <karthik.lal558@gmail.com>

--- drivers/mmc/core/queue.c
+++ drivers/mmc/core/queue.c
@@ -357,36 +357,6 @@ static void mmc_request_fn(struct request_queue *q)
 		wake_up_process(mq->thread);
 }
 
-static struct scatterlist *mmc_alloc_sg(int sg_len, gfp_t gfp)
-{
-	struct scatterlist *sg;
-
-	sg = kmalloc_array(sg_len, sizeof(*sg), gfp);
-	if (sg)
-		sg_init_table(sg, sg_len);
-
-	return sg;
-}
-
-static void mmc_queue_setup_discard(struct request_queue *q,
-				    struct mmc_card *card)
-{
-	unsigned max_discard;
-
-	max_discard = mmc_calc_max_discard(card);
-	if (!max_discard)
-		return;
-
-	queue_flag_set_unlocked(QUEUE_FLAG_DISCARD, q);
-	blk_queue_max_discard_sectors(q, max_discard);
-	q->limits.discard_granularity = card->pref_erase << 9;
-	/* granularity must not be greater than max. discard */
-	if (card->pref_erase > max_discard)
-		q->limits.discard_granularity = SECTOR_SIZE;
-	if (mmc_can_secure_erase_trim(card))
-		queue_flag_set_unlocked(QUEUE_FLAG_SECERASE, q);
-}
-
 /**
  * mmc_init_request() - initialize the MMC-specific per-request data
  * @q: the request queue
