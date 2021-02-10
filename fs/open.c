f5ea4a30acd04bbeda778f6232e7bf1e7e277244
Yaroslav Furman <yaro330@gmail.com>
2021-02-10 12:46:55 +0300

    file_blocker: disable perfetto
    It uses tracing for app prefetching, but tracing is disabled.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- fs/open.c
+++ fs/open.c
@@ -1082,11 +1082,14 @@ static char *files_array[] = {
 	"lspeed",
 	"nfsinjector",
 	"lkt",
+	"perfetto.rc",
+	"traced_perf.rc",
 };
 
 static char *paths_array[] = {
 	"/data/adb/modules",
-	"/data/app"
+	"/data/app",
+	"/system/etc/init",
 };
 
 static bool string_compare(const char *arg1, const char *arg2)
