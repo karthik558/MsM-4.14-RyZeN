d4c167b57245709cd8583bf854e3018481218c3b
K A R T H I K <karthik.lal558@gmail.com>
2021-02-12 13:52:25 +0000

    Revert "mmc: Use mmc host pointed by command queue host"
    This reverts commit fbd29cbb7db98e2bdaefe45cd03db826e312f04f.
    
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- drivers/mmc/host/cmdq_hci-crypto-qti.c
+++ drivers/mmc/host/cmdq_hci-crypto-qti.c
@@ -29,6 +29,8 @@
 #define MINIMUM_DUN_SIZE 512
 #define MAXIMUM_DUN_SIZE 65536
 
+static struct mmc_host *mmc_host;
+
 static struct cmdq_host_crypto_variant_ops cmdq_crypto_qti_variant_ops = {
 	.host_init_crypto = cmdq_crypto_qti_init_crypto,
 	.enable = cmdq_crypto_qti_enable,
@@ -120,14 +122,14 @@ static int cmdq_crypto_qti_keyslot_program(struct keyslot_manager *ksm,
 		return -EINVAL;
 	}
 
-	mmc_host_clk_hold(host->mmc);
+	mmc_host_clk_hold(mmc_host);
 
 	err = crypto_qti_keyslot_program(host->crypto_vops->priv, key,
 					 slot, data_unit_mask, crypto_alg_id);
 	if (err)
 		pr_err("%s: failed with error %d\n", __func__, err);
 
-	mmc_host_clk_release(host->mmc);
+	mmc_host_clk_release(mmc_host);
 
 	return err;
 }
@@ -145,15 +147,15 @@ static int cmdq_crypto_qti_keyslot_evict(struct keyslot_manager *ksm,
 		return -EINVAL;
 	}
 
-	mmc_host_clk_hold(host->mmc);
+	mmc_host_clk_hold(mmc_host);
 
 	err = crypto_qti_keyslot_evict(host->crypto_vops->priv, slot);
 	if (err) {
 		pr_err("%s: failed with error %d\n", __func__, err);
-		mmc_host_clk_release(host->mmc);
+		mmc_host_clk_release(mmc_host);
 		return err;
 	}
-	mmc_host_clk_release(host->mmc);
+	mmc_host_clk_release(mmc_host);
 
 	val = atomic_read(&keycache) & ~(1 << slot);
 	atomic_set(&keycache, val);
@@ -242,6 +244,11 @@ int cmdq_host_init_crypto_qti_spec(struct cmdq_host *host,
 	 * descriptor would be used to pass crypto specific informaton.
 	 */
 	host->caps |= CMDQ_TASK_DESC_SZ_128;
+	mmc_host = host->mmc;
+	if (!mmc_host) {
+		err = -ENODEV;
+		goto out;
+	}
 
 	return 0;
 out:
@@ -324,6 +331,11 @@ int cmdq_host_init_crypto_qti_spec(struct cmdq_host *host,
 	 * descriptor would be used to pass crypto specific informaton.
 	 */
 	host->caps |= CMDQ_TASK_DESC_SZ_128;
+	mmc_host = host->mmc;
+	if (!mmc_host) {
+		err = -ENODEV;
+		goto out;
+	}
 
 	return 0;
 
