8c4a22598a5014468c66fadf380e3929d974b2ad
K A R T H I K <karthik.lal558@gmail.com>
2022-01-29 11:53:41 -0500

    drivers/media: dmxdev: add back struct dmx_caps/caps entries
    * This got removed while upstreming kernel (https://github.com/karthik558/MsM-4.14-RyZeN-/commit/3536e6889b9db7e7b6d1c18ec907501d00cc27fe#diff-3a884247046a05b3d71b3da119992b771a2bedaa9e86ef1875eebfa73144e0c4L4846)
    * Also this fixes build_error facing after upstreaming to vX.263.
    
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- drivers/media/dvb-core/dmxdev.c
+++ drivers/media/dvb-core/dmxdev.c
@@ -4843,6 +4843,7 @@ static const struct file_operations dbgfs_filters_fops = {
 int dvb_dmxdev_init(struct dmxdev *dmxdev, struct dvb_adapter *dvb_adapter)
 {
 	int i, ret;
+	struct dmx_caps caps;
 
 	if (dmxdev->demux->open(dmxdev->demux) < 0)
 		return -EUSERS;
